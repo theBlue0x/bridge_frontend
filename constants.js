d87c97e43b51e30fb1a530739250432c7af973f3
theBlue0x <theBlue0x@users.noreply.github.com>
2022-08-09 14:27:06 -0400

    Use remote node for testing

--- constants.js
+++ constants.js
@@ -2,11 +2,11 @@ var Constants = (function(Constants, $, undefined) {
   
   Constants.USDXId = '14151369516674261956'; // Testnet USDX
 
-  Constants.apiUrl = 'https://testnet.blue0x.com:6876/nxt'; // Testnet Node
+  Constants.apiUrl = 'https://testnet.blue0x.com:6877/nxt'; // Testnet Node
 
   Constants.lockWallet = 'BLX-FLLY-DRG6-TUDB-ESVU2';  // Testnet Lock Wallet
 
-  Constants.mintAPI = 'http://localhost:3000/mint' // Testnet verify API
+  Constants.mintAPI = 'https://localhost:3000/mint' // Testnet verify API
 
   return Constants;
 
