f39ab3c0976e6ff9a24aaef325e461f5e87c4d57
Blue0x.com <3836882+theBlue0x@users.noreply.github.com>
2022-08-11 22:40:53 -0400

    Update constants.js

--- constants.js
+++ constants.js
@@ -2,11 +2,11 @@ var Constants = (function(Constants, $, undefined) {
   
   Constants.USDXId = '14151369516674261956'; // Testnet USDX
 
-  Constants.apiUrl = 'http://localhost:6876/nxt'; // Testnet Node
+  Constants.apiUrl = 'https://testnet.blue0x.com:6877/nxt'; // Testnet Node
 
   Constants.lockWallet = 'BLX-FLLY-DRG6-TUDB-ESVU2';  // Testnet Lock Wallet
 
-  Constants.mintAPI = 'http://localhost:3000/mint'
+  Constants.mintAPI = 'https://localhost:3000/mint'
 
   return Constants;
 
